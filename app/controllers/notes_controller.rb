608bb30b7cd768daeb4420982195430251c58d58
luke bickle <lukebickle@yahoo.com>
2015-03-13 22:24:25 -0500

    Edit & Destroy Notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -22,8 +22,15 @@ class NotesController < ApplicationController
   def edit
   end
   def update
+    if @note.update(note_params)
+      redirect_to @note
+    else
+      render 'edit'
+    end
   end
   def destroy
+    @note.destroy
+    redirect_to notes_path
   end
 
   private
